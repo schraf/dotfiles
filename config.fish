4585e06338c84a613ee87841b4ca48a217c1b0b7
Marc Schraffenberger <schraf@macbookair.lan>
2025-10-10 09:08:49 -0400

    cleaning up fish config

--- config.fish
+++ config.fish
@@ -1,49 +1,35 @@
-# Paths
-fish_add_path $HOME/.local/bin
-fish_add_path $HOME/.cargo/bin
-
-# Environment Variables
-set -x TINTED_TMUX_OPTION_ACTIVE 1
-set -x TINTED_TMUX_OPTION_ACTIVE 1
-
-# zoxide
-zoxide init fish | source
-
-# starship prompt
-starship init fish | source
-
-# Better history management
+#--==========================================================
+#--=== Fish settings
+#--==========================================================
 set -g fish_history_size 10000
 set -g fish_history_path ~/.local/share/fish/fish_history
-
-# Better completion
 set -g fish_complete_path ~/.config/fish/completions
 
-# Aliases for common commands
+#--==========================================================
+#--=== Source tool configs
+#--==========================================================
+zoxide init fish | source
+starship init fish | source
+fzf --fish | source
+
+#--==========================================================
+#--=== Command Aliases
+#--==========================================================
 alias ll "eza -la --git"
 alias la "eza -la"
 alias l "eza -l"
 alias .. "cd .."
 alias ... "cd ../.."
 alias .... "cd ../../.."
-
-
-# Better cat with syntax highlighting
 alias cat "bat"
-
-# Better ls
 alias ls "eza"
-
-# Better find
 alias find "fd"
-
-# Better grep
 alias grep "rg"
-
-# Better man pages
 alias man "tldr"
 
-# Base16 Shell
+#--==========================================================
+#--=== Color Scheme
+#--==========================================================
 if status --is-interactive
     set BASE16_SHELL "$HOME/.config/base16-shell/"
     source "$BASE16_SHELL/profile_helper.fish"
