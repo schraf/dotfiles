6a11b87127af677c55446a017c348db9f9b7f466
Marc Schraffenberger <marc@schraffenberger.com>
2025-10-09 22:51:31 -0400

    adding zoxide to fish config

--- config.fish
+++ config.fish
@@ -6,6 +6,9 @@ fish_add_path $HOME/.cargo/bin
 set -x TINTED_TMUX_OPTION_ACTIVE 1
 set -x TINTED_TMUX_OPTION_ACTIVE 1
 
+# zoxide
+zoxide init fish | source
+
 # Base16 Shell
 if status --is-interactive
     set BASE16_SHELL "$HOME/.config/base16-shell/"
