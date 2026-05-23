a4f0dc54c894763a7131eadf7615c383ec8d4592
Marc Schraffenberger <marc@schraffenberger.com>
2026-05-22 21:08:52 -0500

    adding fastmod removing aichat and delta

--- packages.mk
+++ packages.mk
@@ -1,19 +1,16 @@
 # List of packages that have the same name across all supported package managers
-COMMON_PKGS := vim tmux fzf lazygit fish zoxide ripgrep bat eza starship btop lazydocker jq glow aichat opencode boxes
+COMMON_PKGS := vim tmux fzf lazygit fish zoxide ripgrep bat eza starship btop lazydocker jq glow opencode boxes fastmod
 
 # Packages with different names depending on the package manager
 # Default to brew/pacman names
 FD_PKG := fd
-DELTA_PKG := delta
 
 # Overrides for apt, yum, and dnf
 ifeq ($(PACKAGE_MANAGER),apt)
     FD_PKG := fd-find
-    DELTA_PKG := git-delta
 else ifneq ($(filter $(PACKAGE_MANAGER),yum dnf),)
     FD_PKG := fd-find
-    DELTA_PKG := git-delta
 endif
 
 # The final combined list of packages to install
-PACKAGE_LIST := $(COMMON_PKGS) $(FD_PKG) $(DELTA_PKG)
+PACKAGE_LIST := $(COMMON_PKGS) $(FD_PKG) 
